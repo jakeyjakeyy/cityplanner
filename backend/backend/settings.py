975de98b7f27e0ff88943c5c3e09da9f083f8874
jakeyjakeyy <jrichca1@gmail.com>
2023-12-11 14:59:36 -0800

    basic setup and configs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cityplanner',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
