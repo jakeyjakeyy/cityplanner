80f278fae39348778967a0b59f108cf42bf1f191
jakeyjakeyy <jrichca1@gmail.com>
2024-01-02 19:04:15 -0800

    results scroller

--- frontend/src/components/SearchResultsContainer.tsx
+++ frontend/src/components/SearchResultsContainer.tsx
@@ -9,58 +9,44 @@ const SearchResultCardContainer = ({
   handleMouseEnter,
 }: any) => {
   return (
-    <div className="searchResultsContainer">
-      <div className="searchResults">
-        {searchResults?.searchResults.places.map(
-          (place: any, index: number) => (
-            <div
-              className="searchResultCardContainer"
-              key={place.displayName.text}
-              onClick={handleSelection}
-              id={searchResults?.searchResults?.places
-                ?.indexOf(place)
-                ?.toString()}
-              onMouseEnter={() => handleMouseEnter(index)}
+    <div className="searchResults">
+      {searchResults?.searchResults.places.map((place: any, index: number) => (
+        <div
+          className="searchResultCardContainer"
+          key={place.displayName.text}
+          onClick={handleSelection}
+          id={searchResults?.searchResults?.places?.indexOf(place)?.toString()}
+          onMouseEnter={() => handleMouseEnter(index)}
 
-              // onMouseLeave={() => setTempMapItem({})}
-            >
-              <SearchResultCard item={place} type="place" index={index} />
-            </div>
-          )
-        )}
-        {searchResults?.events ? (
-          searchResults?.events?.ticketmaster.map(
-            (event: any, index: number) => (
-              <div
-                className="eventResultCard"
-                id={searchResults?.events?.ticketmaster
-                  .indexOf(event)
-                  ?.toString()}
-                onClick={handleSelectionEvent}
-                onMouseEnter={() => handleMouseEnter(index)}
-              >
-                <SearchResultCard
-                  item={event}
-                  type="ticketmaster"
-                  index={index}
-                />
-              </div>
-            )
-          )
-        ) : (
-          <div></div>
-        )}
-        {searchResults?.events?.seatgeek.map((event: any, index: number) => (
+          // onMouseLeave={() => setTempMapItem({})}
+        >
+          <SearchResultCard item={place} type="place" index={index} />
+        </div>
+      ))}
+      {searchResults?.events ? (
+        searchResults?.events?.ticketmaster.map((event: any, index: number) => (
           <div
             className="eventResultCard"
-            id={searchResults?.events?.seatgeek.indexOf(event)?.toString()}
+            id={searchResults?.events?.ticketmaster.indexOf(event)?.toString()}
             onClick={handleSelectionEvent}
             onMouseEnter={() => handleMouseEnter(index)}
           >
-            <SearchResultCard item={event} type="seatgeek" index={index} />
+            <SearchResultCard item={event} type="ticketmaster" index={index} />
           </div>
-        ))}
-      </div>
+        ))
+      ) : (
+        <div></div>
+      )}
+      {searchResults?.events?.seatgeek.map((event: any, index: number) => (
+        <div
+          className="eventResultCard"
+          id={searchResults?.events?.seatgeek.indexOf(event)?.toString()}
+          onClick={handleSelectionEvent}
+          onMouseEnter={() => handleMouseEnter(index)}
+        >
+          <SearchResultCard item={event} type="seatgeek" index={index} />
+        </div>
+      ))}
     </div>
   );
 };
