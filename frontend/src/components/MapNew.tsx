cb6329f6d4d3731eef74359d15f3519d2640fabc
jakeyjakeyy <jrichca1@gmail.com>
2024-01-10 13:42:59 -0800

    map dark mode

--- frontend/src/components/MapNew.tsx
+++ frontend/src/components/MapNew.tsx
@@ -3,6 +3,87 @@ import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 import "./Map.css";
 const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
+const darkModeStyle = [
+  { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+  { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+  { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+  {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [{ color: "#263c3f" }],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#6b9a76" }],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [{ color: "#38414e" }],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#212a37" }],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#9ca5b3" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [{ color: "#746855" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#1f2835" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#f3d19c" }],
+  },
+  {
+    featureType: "transit",
+    elementType: "geometry",
+    stylers: [{ color: "#2f3948" }],
+  },
+  {
+    featureType: "transit.station",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [{ color: "#17263c" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#515c6d" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.stroke",
+    stylers: [{ color: "#17263c" }],
+  },
+];
+
 function NewMap({ tempMapItem }: any) {
   // Default to Portland, OR
   let position = {
@@ -13,7 +94,12 @@ function NewMap({ tempMapItem }: any) {
   if (Object.keys(tempMapItem).length === 0) {
     return (
       <APIProvider apiKey={apiKey || ""}>
-        <Map className="map" center={position} zoom={12} />
+        <Map
+          className="map"
+          center={position}
+          zoom={12}
+          styles={darkModeStyle}
+        />
       </APIProvider>
     );
   } else {
@@ -21,14 +107,14 @@ function NewMap({ tempMapItem }: any) {
       lat: tempMapItem.location.latitude,
       lng: tempMapItem.location.longitude,
     };
+    return (
+      <APIProvider apiKey={apiKey || ""}>
+        <Map className="map" center={position} zoom={16}>
+          <Marker position={position} />
+        </Map>
+      </APIProvider>
+    );
   }
-  return (
-    <APIProvider apiKey={apiKey || ""}>
-      <Map className="map" center={position} zoom={16}>
-        <Marker position={position} />
-      </Map>
-    </APIProvider>
-  );
 }
 
 export default NewMap;
